438c65fd26cefaa08ea16b9a18205389e30849eb
wolveshowl <dipper25@naver.com>
2025-08-24 20:24:48 +0900

    init

---
+++ src/main/java/com/app/gabboo/GabbooApplication.java
@@ -0,0 +1,13 @@
+package com.app.gabboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabbooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/gabboo/GabbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gabboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
