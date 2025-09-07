6529421a649f441e3f797c1cd6c3fb398aa556a0
wolveshowl <dipper25@naver.com>
2025-09-08 01:00:58 +0900

    FEAT: ãã패스워드 암호화 클래스 빈 추가

--- src/main/java/com/app/gabboo/global/config/SecurityConfig.java
+++ src/main/java/com/app/gabboo/global/config/SecurityConfig.java
@@ -4,9 +4,16 @@ import com.app.gabboo.users.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2-console/**", "/oauth2/**").permitAll()
+                .requestMatchers("/h2-console/**", "/oauth2/**", "/api/**").permitAll()
                 .anyRequest().authenticated()
         )
             .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")
@@ -26,5 +33,15 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .oauth2Login(oauth -> oauth.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService)));
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
